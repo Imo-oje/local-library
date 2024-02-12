63022e5e8e141eb3ae299130a6df722599aa84e4
imooje <savvyplus020@gmail.com>
2024-02-13 00:49:31 +0100

    feat: env

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 // Set up mongoose connection
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 mongoose.set("strictQuery", false);
 
-const dev_db_url =
-  "mongodb+srv://savvyplus018:imooje@library.yxkliql.mongodb.net/local_library?retryWrites=true&w=majority";
-
+const dev_db_url = process.env.MONGODB_DEV;
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 dataBase().catch((err) => console.log(err));
